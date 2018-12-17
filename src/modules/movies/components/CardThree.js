4b27fbbf0408b8c23eb4fdfeadbf558bf5a37b2f
lam phan <lamtrongphan@gmail.com>
2018-12-17 10:40:10 +0700

    implement the menu using redux

--- src/modules/movies/components/CardThree.js
+++ src/modules/movies/components/CardThree.js
@@ -1,5 +1,6 @@
 /* eslint-disable new-cap */
-import React, { PropTypes, Component } from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import {
 	Image,
 	Text,
